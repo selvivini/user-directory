11c05bcfad25da2a717089e94f9f5a8d38f4d9f3
vinitha <selvivini@gmail.com>
2021-04-20 18:46:34 -0500

    removed the outer folder

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div className= "jumbotron  text-warning h1" style = {{background: "#9B30FF"}}>
+         <i class="fal fa-address-card"></i> USER-DIRECTORY
+        </div>
+    )
+}
+
+export default header
